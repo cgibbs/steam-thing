cf03720143dd5fa7d740c7f37ea65db839a02173
Chance Gibbs <kcg005@shsu.edu>
2023-02-05 00:27:48 -0600

    gitignore update, more heroku stuff, install command for server package.json

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { default: axios } = require("axios");
+const axios = require("axios");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 
@@ -12,8 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.get("/steamNews/:limit", (req, res) => {
-  axios
-    .get(
+  axios.get(
       "https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=" +
         req.params.limit +
         "&maxlength=300&format=json"
