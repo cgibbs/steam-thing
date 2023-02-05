218834fcea27bb20831ac27da8363a0b0f4fc2bc
Chance Gibbs <kcg005@shsu.edu>
2023-02-05 00:03:26 -0600

    updated package.json and server code for Heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const https = require("https");
 const express = require("express");
 const { default: axios } = require("axios");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get("/steamNews/:limit", (req, res) => {
   axios
